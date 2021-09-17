757369675259dc22f3e382194c8f6ca545bb65f3
BeatrizSi <bia.netadm2015@gmail.com>
2021-09-17 18:27:03 -0300

    Modificação Tela Cadastro

--- lib/screens/tela_cadastro.dart
+++ lib/screens/tela_cadastro.dart
@@ -9,6 +9,56 @@ class TelaCadastro extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Cadastro de usuario"),
       ),
+
+      body: Column(
+        children: <Widget>[
+          _Form(),
+        ],
+      ),
+
     );
+
+
+}
+  Widget _Form() {
+
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            new TextFormField(
+        decoration: new InputDecoration(hintText: 'Nome:'),
+        maxLength: 40,
+            ),
+
+            new TextFormField(
+                decoration: new InputDecoration(hintText: 'Número para contato:'),
+                keyboardType: TextInputType.phone,
+                maxLength: 15,
+                ),
+            new TextFormField(
+              decoration: new InputDecoration(hintText: 'Escreva o endereço:'),
+              maxLength: 40,
+            ),
+
+            new RaisedButton(
+              onPressed: _sendForm,
+                color: Colors.deepOrange,
+              child: new Text('Salvar'),
+            )
+
+          ],
+        ),
+      ),
+    );
+
+
   }
+
+  _sendForm() {
+
+  }
+
 }
