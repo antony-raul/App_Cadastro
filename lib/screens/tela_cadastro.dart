baa38c64ed1accf9f99b88554c8e5a88491ba6f0
antony-raul <raulzin.raul@gmail.com>
2021-09-18 15:18:39 -0300

    floating button

--- lib/screens/tela_cadastro.dart
+++ lib/screens/tela_cadastro.dart
@@ -1,64 +1,59 @@
 import 'package:flutter/material.dart';
 
 class TelaCadastro extends StatelessWidget {
-  const TelaCadastro({ Key? key }) : super(key: key);
+  const TelaCadastro({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Cadastro de usuario"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {},
+          )
+        ],
       ),
-
       body: Column(
         children: <Widget>[
           _Form(),
         ],
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save),
+      ),
     );
+  }
 
-
-}
   Widget _Form() {
-
     return Expanded(
       flex: 1,
       child: Container(
         color: Colors.white,
         child: Column(
           children: <Widget>[
-            new TextFormField(
-        decoration: new InputDecoration(hintText: 'Nome:'),
-        maxLength: 40,
-            ),
-
-            new TextFormField(
-                decoration: new InputDecoration(hintText: 'Número para contato:'),
-                keyboardType: TextInputType.phone,
-                maxLength: 15,
-                ),
-            new TextFormField(
-              decoration: new InputDecoration(hintText: 'Escreva o endereço:'),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Nome:'),
               maxLength: 40,
             ),
-
-            new RaisedButton(
-              onPressed: _sendForm,
-                color: Colors.deepOrange,
-              child: new Text('Salvar'),
-            )
-
+            TextFormField(
+              decoration:
+                  const InputDecoration(hintText: 'Número para contato:'),
+              keyboardType: TextInputType.phone,
+              maxLength: 15,
+            ),
+            TextFormField(
+              decoration:
+                  const InputDecoration(hintText: 'Escreva o endereço:'),
+              maxLength: 40,
+            ),
           ],
         ),
       ),
     );
-
-
   }
 
-  _sendForm() {
-
-  }
-
+  _sendForm() {}
 }
